e153d7e442e5b3fa8bf4615ae2bd54fcea51973c
anaserrami <anaserrami24@gmail.com>
2024-08-30 15:49:02 +0100

    add routes to login and register pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './sections/dashboard';
+import Register from '@/pages/register.tsx';
+import Login from '@/pages/login.tsx';
 
 const RoutesWrapper = () => {
   // const { token } = useAuthStore();
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/register" element={<Register />} />
       <Route path="*" element={<Dashboard />} />
     </Routes>
   );
